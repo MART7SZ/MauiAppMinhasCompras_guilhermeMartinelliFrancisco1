1aca09e85fb93a1f67beef6fee63a1d290fea244
GuilhermeMartinelliFrancisco <guiguimartinellifrancisco16@gmail.com>
2026-03-09 19:29:50 -0300

    Implementação de busca dinâmica com ObservableCollection

--- Views/ListarProduto.cs
+++ Views/ListarProduto.cs
@@ -1,12 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MauiAppMinhasCompras.Models;
+using System.Collections.ObjectModel;
 
 namespace MauiAppMinhasCompras.Views
 {
-    internal class ListarProduto
+    public class ListarProduto : ContentPage
     {
+        SearchBar busca;
+        ListView listaProdutos;
+
+        ObservableCollection<Produto> produtos;
+        ObservableCollection<Produto> produtosFiltrados;
+
+        public ListarProduto()
+        {
+            Title = "Lista de Produtos";
+
+            busca = new SearchBar
+            {
+                Placeholder = "Buscar produto..."
+            };
+
+            busca.TextChanged += SearchBar_TextChanged;
+
+            listaProdutos = new ListView
+            {
+                ItemTemplate = new DataTemplate(() =>
+                {
+                    TextCell cell = new TextCell();
+                    cell.SetBinding(TextCell.TextProperty, "Descricao");
+                    cell.SetBinding(TextCell.DetailProperty, "Quantidade");
+                    return cell;
+                })
+            };
+
+            Content = new StackLayout
+            {
+                Padding = 10,
+                Children =
+                {
+                    busca,
+                    listaProdutos
+                }
+            };
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var lista = await App.Db.GetAll();
+
+            produtos = new ObservableCollection<Produto>(lista);
+
+            produtosFiltrados = new ObservableCollection<Produto>(produtos);
+
+            listaProdutos.ItemsSource = produtosFiltrados;
+        }
+
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string texto = e.NewTextValue?.ToLower() ?? "";
+
+            produtosFiltrados.Clear();
+
+            foreach (var item in produtos)
+            {
+                if (item.Descricao.ToLower().Contains(texto))
+                {
+                    produtosFiltrados.Add(item);
+                }
+            }
+        }
     }
 }
